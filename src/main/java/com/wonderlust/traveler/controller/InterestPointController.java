8b1aa8c12786292bb6a60e02a596110336447055
Ignatius Damai <ignatiusdamai@gmail.com>
2014-10-19 07:59:43 -0400

    Added Interest PointController

---
+++ src/main/java/com/wonderlust/traveler/controller/InterestPointController.java
@@ -0,0 +1,40 @@
+package com.wonderlust.traveler.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wonderlust.traveler.model.InterestPoint;
+import com.wonderlust.traveler.service.InterestPointService;
+
+@Controller
+@RequestMapping(value = "/api/interest-point")
+public class InterestPointController {
+
+	@Autowired
+	InterestPointService interestPointService;
+
+	@ResponseBody
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	InterestPoint create(@RequestParam String id) {
+		return interestPointService.getInterestPointById(id);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/add-point", method = RequestMethod.POST)
+	InterestPoint updatePoint(@RequestParam String id,
+			@RequestParam String videoToken, @RequestParam String address,
+			@RequestBody String description) {
+		InterestPoint interestPoint = interestPointService
+				.getInterestPointById(id);
+		interestPoint.setAddress(address);
+		interestPoint.setDescription(description);
+		interestPoint.setVideoToken(videoToken);
+		interestPointService.updateInterestPoint(interestPoint);
+		return interestPoint;
+	}
+}
